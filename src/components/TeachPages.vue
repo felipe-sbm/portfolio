ca5da00223551c4a20aaf7da52c26a697e0b5a12
felipe-sbm <felipesbm2005@gmail.com>
2024-09-02 17:29:38 -0300

    feat: Improve UI by optimizing image loading

--- src/components/TeachPages.vue
+++ src/components/TeachPages.vue
@@ -9,7 +9,7 @@
                         <div
                             class="h-16 w-16 bg-contain rounded-full bg-[url(https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiuQlW456yzxQL4b801-jzQEgdmJ6TVM6Wy6ahPg6nYQzxWcJs7hYAC-Zln5QCRNE7ZBIcKb0vY8LXrTS9di8CLjwC8Jp5DJx8jTQ_6yQKj27komW56tuU6OGB-x5Xi2zbJ9olkyyVTJ3REiPXQpW8_6OsDsN0dugPeNMrwIPj_uwaP5g/s220/eu.png)]" />
                         <div
-                            class="absolute right-0 h-4 w-4 bg-contain bg-[url(https://www.blogger.com/img/logo-16.png)]" />
+                            class="absolute right-0 h-4 w-4 group-hover:w-6 group-hover:h-6 group-hover:rotate-12 duration-300 bg-contain bg-[url(https://www.blogger.com/img/logo-16.png)]" />
                     </div>
                     <div class="mt-0.5 text-md font-medium text-zinc-900 dark:text-zinc-300">
                         <h3 class="text-sm font-mono font-light text-zinc-800 dark:text-zinc-400">
@@ -33,7 +33,7 @@
                         <div
                             class="h-16 w-16 bg-contain rounded-full bg-[url(https://yt3.googleusercontent.com/CdGDVFNuEeu1QjSOQK22bf23eJxhMaqTjsRASdQUSVxOTVOxJ5z00rY_ngV_BuiON8OxQOtJkA=s160-c-k-c0x00ffffff-no-rj)]" />
                         <div
-                            class="absolute right-0 h-5 w-5 bg-contain bg-[url(https://yt3.ggpht.com/KymSHwxchJ4rKx_vDoS6am_u_CHpyx-x8OAsFBuLRRhMbyjYqeeeMdn8zSJkrv8Kqtwho-CUMEs3=s256-c-fcrop64=1,00000000ffffffff-nd-v1)]" />
+                            class="absolute right-0 h-5 w-5 group-hover:w-6 group-hover:h-6 group-hover:rotate-12 duration-300 bg-contain bg-[url(https://yt3.ggpht.com/KymSHwxchJ4rKx_vDoS6am_u_CHpyx-x8OAsFBuLRRhMbyjYqeeeMdn8zSJkrv8Kqtwho-CUMEs3=s256-c-fcrop64=1,00000000ffffffff-nd-v1)]" />
                     </div>
                     <div class="mt-0.5 text-md font-medium text-zinc-900 dark:text-zinc-300">
                         <h3 class="text-sm font-mono font-light text-zinc-800 dark:text-zinc-400">
